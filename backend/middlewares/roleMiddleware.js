1e453bf379a5618bd62c77f9eca67df72b2b3ac0
Rodrigo <rgutierrezvielma@gmail.com>
2025-06-15 21:36:59 -0400

    Roles y permisos en backend

---
+++ backend/middlewares/roleMiddleware.js
@@ -0,0 +1,10 @@
+const requireRole = (requiredRole) => {
+  return (req, res, next) => {
+    if (!req.user || req.user.role !== requiredRole) {
+      return res.status(403).json({ error: 'No autorizado: rol insuficiente' });
+    }
+    next();
+  };
+};
+
+module.exports = { requireRole };
--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { createEvent, getEvents } = require('../controllers/eventController');
 const verifyToken = require('../middlewares/authMiddleware')
+const { requireRole } = require('../middlewares/roleMiddleware');
 
+router.post('/', verifyToken, requireRole('organizador'), createEvent);
 router.post('/', verifyToken, createEvent);
 router.get('/', getEvents);
 
