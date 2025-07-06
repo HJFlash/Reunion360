1f700c07872250f5e4ee5b3d7f843fe727281c35
Rodrigo <rgutierrezvielma@gmail.com>
2025-07-05 20:14:48 -0400

    Cambios en server.js para render

--- backend/utils/firebase.js
+++ backend/utils/firebase.js
@@ -1,5 +1,17 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+const path = require('path');
+const fs = require('fs');
+
+let serviceAccount;
+
+if (process.env.RENDER) {
+  // En Render, leer desde el archivo secreto
+  const servicePath = '/etc/secrets/serviceAccountKey.json';
+  serviceAccount = JSON.parse(fs.readFileSync(servicePath, 'utf8'));
+} else {
+  // En local, usar el archivo del proyecto
+  serviceAccount = require('./serviceAccountKey.json');
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
